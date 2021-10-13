f77e4fb8e490bf18bccda2eabd69c1baad26a180
giovalgas <giovalgascom@gmail.com>
2021-10-13 01:46:01 -0300

    Update ConfigManager values

--- src/main/java/me/giodev/spleefarena/data/config/ConfigManager.java
+++ src/main/java/me/giodev/spleefarena/data/config/ConfigManager.java
@@ -1,6 +1,7 @@
 package me.giodev.spleefarena.data.config;
 
 import me.giodev.spleefarena.SpleefArena;
+import org.bukkit.Location;
 import org.bukkit.configuration.InvalidConfigurationException;
 
 public class ConfigManager {
@@ -10,6 +11,12 @@ public class ConfigManager {
 
   //Config Values
   private String consolePrefix;
+  private int resetTimer;
+  private Integer[] resetLocation;
+  private int deathHeight;
+  private int snowballStartAmount;
+  private int snowballBreakAmount;
+
 
   public ConfigManager(SpleefArena plugin) throws InvalidConfigurationException {
     this.plugin = plugin;
@@ -21,6 +28,11 @@ public class ConfigManager {
     config.load();
 
     this.consolePrefix = config.getString(ConfigKeys.CONSOLE_PREFIX, ConfigDefaults.CONSOLE_PREFIX);
+    this.resetTimer = config.getInt(ConfigKeys.RESET_TIMER, ConfigDefaults.RESET_TIMER);
+    this.resetLocation = config.getIntegerList(ConfigKeys.RESET_LOCATION, ConfigDefaults.RESET_LOCATION);
+    this.deathHeight = config.getInt(ConfigKeys.DEATH_HEIGHT, ConfigDefaults.DEATH_HEIGHT);
+    this.snowballStartAmount = config.getInt(ConfigKeys.SNOWBALLS_START_AMOUNT, ConfigDefaults.SNOWBALLS_START_AMOUNT);
+    this.snowballBreakAmount = config.getInt(ConfigKeys.SNOWBALLS_BREAK_AMOUNT, ConfigDefaults.SNOWBALLS_BREAK_AMOUNT);
 
   }
 
@@ -31,5 +43,26 @@ public class ConfigManager {
   public String getConsolePrefix() {
     return consolePrefix;
   }
+
+  public int getResetTimer() {
+    return resetTimer;
+  }
+
+  public Integer[] getResetLocation() {
+    return resetLocation;
+  }
+
+  public int getDeathHeight() {
+    return deathHeight;
+  }
+
+  public int getSnowballStartAmount() {
+    return snowballStartAmount;
+  }
+
+  public int getSnowballBreakAmount() {
+    return snowballBreakAmount;
+  }
+
 
 }
